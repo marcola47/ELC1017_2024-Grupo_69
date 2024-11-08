f41222fae971888c41f5f97008be05431d7e9927
marcola47 <mekaminski47@gmail.com>
2024-11-08 10:11:54 -0300

    .

--- scripts/routing.py
+++ scripts/routing.py
@@ -1,58 +1,131 @@
-# routing.py
-from collections import defaultdict
+import heapq
+from mininet.net import Mininet
+from scapy.all import *
+import threading
+import time
 
-class DistanceVectorRouting:
-    def __init__(self, routers):
-        self.routers = routers
-        self.routing_table = defaultdict(dict)
-        self.initialize_routing()
+# Basic structure of a distance-vector router
+class DistanceVectorRouter:
+    def __init__(self, node, net):
+        self.node = node
+        self.routing_table = {}  # format: {destination: (next_hop, cost)}
+        self.neighbors = net[self.node].connectionsTo(net.hosts)
+        self.lock = threading.Lock()
+        self.periodic_update()
 
-    def initialize_routing(self):
-        for router in self.routers:
-            # Inicializa as tabelas de roteamento com distâncias infinitas
-            self.routing_table[router] = {neighbor: float('inf') for neighbor in self.routers}
-            self.routing_table[router][router] = 0  # Distância para si mesmo é 0
+    def initialize_table(self):
+        # Direct connections initialization
+        for neighbor, intf in self.neighbors:
+            self.routing_table[neighbor] = (neighbor, 1)
 
-    def update_route(self, router, destination, distance):
-        self.routing_table[router][destination] = distance
-
-    def get_best_route(self, router, destination):
-        # Para compatibilidade com Python 3.5, não usamos f-string
-        return min(self.routing_table[router], key=lambda x: self.routing_table[router][x])
-
-    def share_routing_info(self):
-        # Simula a troca de informações entre roteadores vizinhos
-        for router in self.routers:
-            for neighbor in self.routers:
-                if router != neighbor:
-                    # Aqui você implementaria a lógica de atualização da tabela de roteamento
-                    pass
-
-    def display_routing_table(self):
-        for router, table in self.routing_table.items():
-            print("Roteador {}:".format(router))  # Mudado para string formatada compatível com Python 3.5
-            for destination, distance in table.items():
-                print("\tDestino: {}, Distância: {}".format(destination, distance))  # Mesmo ajuste
-
-class LinkStateRouting:
-    def __init__(self, routers):
-        self.routers = routers
-        self.topology = defaultdict(list)
-        self.initialize_topology()
-
-    def initialize_topology(self):
-        # Inicializa a topologia completa para cada roteador
-        for router in self.routers:
-            self.topology[router] = []
-
-    def add_link(self, router1, router2):
-        self.topology[router1].append(router2)
-        self.topology[router2].append(router1)
-
-    def update_topology(self, router, link):
-        # Atualiza a topologia com informações de enlaces
+    def receive_update(self, packet):
+        # Parse and process the distance vector received from neighbors
         pass
 
-    def display_topology(self):
-        for router, neighbors in self.topology.items():
-            print("Roteador {} está conectado a {}".format(router, ', '.join(neighbors)))  # Ajuste similar
+    def send_update(self):
+        with self.lock:
+            for neighbor, _ in self.neighbors:
+                packet = self.create_update_packet()
+                self.node.cmd('ping -c 1 {}'.format(neighbor.IP()))
+                send(packet)
+
+    def periodic_update(self):
+        # Send routing table updates periodically
+        threading.Timer(10.0, self.periodic_update).start()
+        self.send_update()
+
+    def create_update_packet(self):
+        # Construct packet containing distance-vector information
+        packet = IP(dst="255.255.255.255") / UDP() / "DistanceVectorRouting"
+        return packet
+
+    def display_routing_table(self):
+        print("\nRouting Table for {}:".format(self.node.name))
+        print("Destination\tNext Hop\tCost")
+        with self.lock:
+            for destination, (next_hop, cost) in self.routing_table.items():
+                print("{}\t{}\t{}".format(destination, next_hop, cost))
+
+    def periodic_display(self):
+        # Display routing table every 15 seconds
+        self.display_routing_table()
+        threading.Timer(15.0, self.periodic_display).start()
+
+# Main Mininet setup
+def distance_vector_topology():
+    net = Mininet()
+    net.start()
+    routers = [DistanceVectorRouter(node, net) for node in net.hosts]
+
+    try:
+        for router in routers:
+            router.initialize_table()
+            router.periodic_display()  # Starts the periodic display
+    except KeyboardInterrupt:
+        net.stop()
+
+class LinkStateRouter:
+    def __init__(self, node, net):
+        self.node = node
+        self.net = net
+        self.routing_table = {}
+        self.neighbor_costs = {neighbor: 1 for neighbor, _ in net[self.node].connectionsTo(net.hosts)}
+        self.lsa_database = {}  # format: {node: {neighbor: cost}}
+        self.lock = threading.Lock()
+        self.broadcast_lsa()
+
+    def broadcast_lsa(self):
+        # Broadcasts LSA with neighbor information
+        lsa_packet = self.create_lsa_packet()
+        for neighbor, _ in self.neighbor_costs.items():
+            send(lsa_packet, iface=self.node.defaultIntf().name)
+        threading.Timer(10.0, self.broadcast_lsa).start()
+
+    def create_lsa_packet(self):
+        # Create a packet with link-state information
+        return IP(dst="255.255.255.255") / UDP() / "LinkStateAdvertisement"
+
+    def receive_lsa(self, packet):
+        # Process incoming LSAs from neighbors
+        pass
+
+    def update_routing_table(self):
+        # Implement Dijkstra's algorithm to compute shortest paths
+        pass
+
+    def periodic_check(self):
+        # Periodically check for any changes in the network state
+        threading.Timer(5.0, self.periodic_check).start()
+        self.update_routing_table()
+
+    def display_routing_table(self):
+        print("\nRouting Table for {}:".format(self.node.name))
+        print("Destination\tNext Hop\tCost")
+        with self.lock:
+            for destination, (next_hop, cost) in self.routing_table.items():
+                print("{}\t{}\t{}".format(destination, next_hop, cost))
+
+    def display_lsa_database(self):
+        print("\nLSA Database for {}:".format(self.node.name))
+        print("Node\tNeighbor\tCost")
+        with self.lock:
+            for node, neighbors in self.lsa_database.items():
+                for neighbor, cost in neighbors.items():
+                    print("{}\t{}\t{}".format(node, neighbor, cost))
+
+    def periodic_display(self):
+        # Display routing table and LSA database every 15 seconds
+        self.display_routing_table()
+        self.display_lsa_database()
+        threading.Timer(15.0, self.periodic_display).start()
+
+def link_state_topology():
+    net = Mininet()
+    net.start()
+    routers = [LinkStateRouter(node, net) for node in net.hosts]
+
+    try:
+        for router in routers:
+            router.periodic_display()  # Starts the periodic display
+    except KeyboardInterrupt:
+        net.stop()
---
+++ test/create_network.py
@@ -0,0 +1,59 @@
+# create_network.py
+import argparse
+from topologies import LineTopology, RingTopology, StarTopology, MeshTopology, HybridTopology
+from algorithms import DistanceVector, LinkState
+from mininet.net import Mininet
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Run network simulation with specified topology and routing algorithm.")
+    parser.add_argument('-t', '--topology', choices=['line', 'ring', 'star', 'mesh', 'hybrid'], required=True, help="Choose the network topology.")
+    parser.add_argument('-a', '--algorithm', choices=['distance-vector', 'link-state'], required=True, help="Choose the routing algorithm.")
+    return parser.parse_args()
+
+def create_topology(topology_name):
+    if topology_name == 'line':
+        return LineTopology()
+    elif topology_name == 'ring':
+        return RingTopology()
+    elif topology_name == 'star':
+        return StarTopology()
+    elif topology_name == 'mesh':
+        return MeshTopology()
+    elif topology_name == 'hybrid':
+        return HybridTopology()
+    else:
+        raise ValueError("Invalid topology name")
+
+def main():
+    args = parse_args()
+    net = Mininet()
+    
+    # Set up topology
+    topology = create_topology(args.topology)
+    topology.build(net)
+    
+    # Choose the routing algorithm
+    if args.algorithm == 'distance-vector':
+        algorithm = DistanceVector(net)
+    elif args.algorithm == 'link-state':
+        algorithm = LinkState(net)
+    else:
+        raise ValueError("Invalid algorithm name")
+    
+    # Run the chosen routing algorithm
+    algorithm.run()
+    
+    # Start the network
+    net.start()
+    net.pingAll()  # Test connectivity
+
+    print("Testing packet transmission...")
+    for i, host in enumerate(net.hosts):
+        dest_ip = net.hosts[(i + 1) % len(net.hosts)].IP()  # Get IP of next host
+        print("Host {} sending packet to {}".format(host.IP(), dest_ip))
+        host.send_packet(dest_ip)
+
+    net.stop()
+
+if __name__ == '__main__':
+    main()
